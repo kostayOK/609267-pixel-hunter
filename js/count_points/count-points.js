4d148a1f90ea28364f934932f38455937cafda2b
kostayOK <itdvnk@gmail.com>
2018-04-30 23:15:17 +0600

    не очень легкое задание только по одному заданию можно развивать целый курс

---
+++ js/count_points/count-points.js
@@ -0,0 +1,41 @@
+const COUNT_RULES = {
+  /**
+   * Правильный ответ: correctAnswer
+   * Быстрый ответ: quickAnswer
+   * Медленный ответ: slowResponse
+   * За каждую оставшуюся к концу игры жизнь: remainingLife
+   * */
+  correctAnswer: 100,
+  quickAnswer: 50,
+  slowResponse: 50,
+  remainingLife: 50,
+  fastTime: 30,
+  // slowResponseTime: 50
+};
+
+export const countPoints = (answers = [], life = 3) => {
+
+  let points = 0;
+  if (answers.length < 10 || answers.length > 10 || life === 0) {
+    return -1;
+  }
+  for (let i = 0; i < answers.length; i++) {
+    let currentAnswer = answers[i];
+    if (currentAnswer.correctAnswer) {
+      points += COUNT_RULES.correctAnswer;
+      if (currentAnswer.time < COUNT_RULES.fastTime) {
+        points += COUNT_RULES.quickAnswer;
+      } else if (currentAnswer.time > COUNT_RULES.fastTime) {
+        points -= COUNT_RULES.slowResponse;
+      }
+    } else {
+      points -= COUNT_RULES.slowResponse;
+    }
+  }
+
+  if (answers.length === 10) {
+    let resultLifeBonus = COUNT_RULES.remainingLife * life;
+    points += resultLifeBonus;
+  }
+  return points;
+};
