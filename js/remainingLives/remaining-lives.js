9ccc1ed778cfa07dc113bd836382d6e3d494e348
kostayOK <itdvnk@gmail.com>
2018-05-02 14:53:34 +0600

    удалил ненужные файлы

--- js/remainingLives/remaining-lives.js
+++
@@ -1,9 +0,0 @@
-/**
- * remainingLives - оставшихся жизней*/
-
-export const remainingLives = (num) => {
-  if (typeof num !== `number`) {
-    throw new Error(`not number not ok`);
-  }
-  return num;
-};
--- js/score_points/score-points.js
+++
@@ -1,9 +0,0 @@
-/**
- * scorePoints - набранных очков*/
-
-export const scorePoints = (x, y) => {
-  if (typeof x !== `number` && typeof y !== `number`) {
-    throw new Error(`not Array not ok`);
-  }
-  return x + y;
-};
